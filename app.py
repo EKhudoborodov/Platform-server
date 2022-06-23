326dd6dc86549961f5b9ad9ae2bdcb34a4d90e84
EKhudoborodov <vakros.vakros@mail.ru>
2022-06-23 22:13:11 +0300

    ver0.2

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import requests
-from flask import Flask, render_template, request
+import requests, os
+from flask import Flask, render_template, request, redirect, url_for, session
 import psycopg2
-
 app = Flask(__name__)
 
 conn = psycopg2.connect(database="server_db",
@@ -17,19 +16,53 @@ def index():
     return render_template('sign_in.html')
 
 @app.route('/signin/', methods=['POST'])
-def login():
+def signin():
     #if request.method == 'POST':
     username = request.form.get('username')
     password = request.form.get('password')
+    session['login'] = username
     if username != '' or password != '':
         cursor.execute(f"SELECT * FROM public.users WHERE login='{username}' AND password='{password}'", (str(username), str(password)))
         records = list(cursor.fetchall())
         if records != []:
-            return render_template('account.html', full_name=f"Hello, {records[0][1]}!", login=f"Your login:{username}", passw=f"Your password:{password}")
+            return redirect(url_for('.home', username=username))
         else:
             return render_template('sign_in2.html')
     else:
         return render_template('sign_in1.html')
+        
+@app.route('/signup/', methods=['GET'])
+def first():
+    return render_template('sign_up.html')
+
+@app.route('/signup/', methods=['POST'])
+def signup():
+    username = request.form.get('username')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+    cursor.execute(f"SELECT * FROM public.users WHERE login='{username}'", (str(username)))
+    records = list(cursor.fetchall())
+    if username != '':
+        if(records == []):
+            if password1 != '' and password2 != '':
+                if password1 == password2:
+                    cursor.execute(f"INSERT INTO public.users (login, password, role) VALUES ('{username}', '{password1}', 'reader')")
+                    conn.commit()
+                    return redirect('http://127.0.0.1:5000/signin')
+                else:
+                    return render_template('sign_up3.html')
+            else:
+                return render_template('sign_up2.html')
+        else:
+            return render_template('sign_up4.html')
+    else:
+        return render_template('sign_up1.html')
+
+@app.route("/home/", methods=['GET', 'POST'])
+def home():
+    username = session.get('login')
+    return render_template('approved.html', login=f"Your login:{username}")
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(24)
     app.run()
